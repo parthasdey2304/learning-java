ba0d6c0d212b7a2c5465d4cd36d0e121bbb48343
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-01 16:24:57 +0530

    All the necessary changes have been made

---
+++ conditional/if_conditions/if_conditions.java
@@ -0,0 +1,24 @@
+public class if_conditions
+{
+    public static void main(String[] args) {
+
+        // this is a nested if condition
+        
+        int a = 5;
+
+        if(a == 5)
+        {
+            System.out.println("The number is 5");
+        }
+
+        else if(a == 6)
+        {
+            System.out.println("The number is 6");
+        }
+
+        else
+        {
+            System.out.println("The number is not 5 or 6");
+        }
+    }
+}
