fb664bf284966ceaa81c557cc7c25e1fc527572c
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-01 16:27:40 +0530

    All the necessary changes have been made

---
+++ conditional/switch_case/switch_case.java
@@ -0,0 +1,25 @@
+
+public class switch_case
+{
+    public static void main(String[] args) {
+            
+            // this is a switch case
+    
+            int a = 5;
+    
+            switch(a)
+            {
+                case 5:
+                    System.out.println("The number is 5");
+                    break;
+    
+                case 6:
+                    System.out.println("The number is 6");
+                    break;
+    
+                default:
+                    System.out.println("The number is not 5 or 6");
+                    break;
+            }
+    }
+}
