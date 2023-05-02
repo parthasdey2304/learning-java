392ea2dbe99b28491c2db4f3582ab6438ff33136
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-02 12:07:21 +0530

    recursion.java has been added.

---
+++ recursion/recursion.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class recursion{
+
+    static int fact(int n)
+    {
+        return (n==0)? 1 : fact(n-1)*n;
+    }
+
+    static int sumOfFact(int n)
+    {
+        return (n==0)? 0 : fact(n%10) + sumOfFact(n/10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of a number 5 : "+fact(5));
+        System.out.println("Sum of factorial of digits of the number 145 is : " + sumOfFact(145));
+    }
+}
