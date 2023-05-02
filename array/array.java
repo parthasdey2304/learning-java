fbc3ab789896473158d6d7c16f3109f78507ce21
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-02 18:21:04 +0530

    array.java has been added

--- array/array.java
+++ array/array.java
@@ -1,1 +1,30 @@
 package array;
+
+public class array{
+
+    static int[] generate(int n)
+    {
+        int arr[] = new int[n];
+
+        for(int i=0; i<n ; i++)
+        {
+            arr[i] = i;
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[5]; // array declaration and initialization
+        
+        // entering values inside the array
+        for(int i : generate(5))
+        {
+            arr[i] = i+1;
+        }
+
+        for(int i = 0; i<5; i++)
+        {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
