ff6c12e71760a7312752026c4e4a7aa2e8cdbb35
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-05-02 18:23:50 +0530

    recursion.java has been added

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -2,14 +2,14 @@ package recursion;
 
 public class recursion{
 
-    static int fact(int n)
+    static int fact(int n) // this method is to return the factorial of a number using recursion
     {
-        return (n==0)? 1 : fact(n-1)*n;
+        return (n == 0)? 1 : fact(n-1) * n;
     }
 
-    static int sumOfFact(int n)
+    static int sumOfFact(int n) // this method is tor return the sum of factorial of the number using recursion
     {
-        return (n==0)? 0 : fact(n%10) + sumOfFact(n/10);
+        return (n == 0)? 0 : fact(n%10) + sumOfFact(n/10);
     }
 
     public static void main(String[] args) {
